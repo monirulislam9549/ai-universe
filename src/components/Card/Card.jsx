a8d4a69a94315f171bd5050b9f9d299d80c34835
monirulislam9549 <monirulislam9549@gmail.com>
2023-03-27 12:07:14 +0600

    see more button implemented

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import Button from '../Button/Button';
 import SingleData from '../SingleData/SingleData';
 
 const Card = () => {
     const [data, setData] = useState([])
+    const [showAll, setShowAll] = useState(false)
+    const handleShowAll = () => {
+        setShowAll(true)
+    }
     useEffect(() => {
         const fetchData = async () => {
             const url = `https://openapi.programming-hero.com/api/ai/tools`
@@ -18,13 +23,19 @@ const Card = () => {
         fetchData()
     }, [])
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-6 lg:px-12'>
-
-            {
-                data.map(singleData => <SingleData
-                    singleData={singleData}
-                    key={singleData.id}
-                ></SingleData>)
+        <div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-6 lg:px-12'>
+                {
+                    data.slice(0, showAll ? 12 : 6).map(singleData => <SingleData
+                        key={singleData.id}
+                        singleData={singleData}
+                    ></SingleData>)
+                }
+            </div>
+            {!showAll &&
+                <span onClick={() => handleShowAll()}>
+                    <Button>See More</Button>
+                </span>
             }
         </div>
     );
